f095a6e90022ac6ce3eae832c2cd5873a0086df1
Romina Moya <rominamoya@gmail.com>
2025-05-09 10:19:09 +0100

    chore: add proper eslint configuration under NextJS 15

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,37 @@
 // eslint.config.js
+import pluginNext from '@next/eslint-plugin-next';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import parser from '@typescript-eslint/parser';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import("eslint").Linter.FlatConfig} */
 export default [
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      prettier: prettierPlugin,
-       
-    },
-    rules: {
-        
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
         },
-      ],
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            prettier: prettierPlugin,
+            '@next/next': pluginNext,
+        },
+        rules: {
+            ...pluginNext.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
+        },
     },
-  },
 ];
